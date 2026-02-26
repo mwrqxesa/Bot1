402860d5adcac72074908b4ea5215e39309da084
mwrqxesa <toconfusaqueroumtempo@gmail.com>
2026-02-26 10:29:05 -0300

    Create botPerms.js

---
+++ config/botPerms.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // Dona do bot (owner)
+  OWNER_ID: '826501596702965850', // Zangwda
+
+  // Admins do bot (pode adicionar vários)
+  BOT_ADMIN_IDS: [
+    '826501596702965850', // Zangwda
+    // '701959666678366229', // exemplo: Zangwdo (se quiser dar admin de bot)
+  ],
+};
